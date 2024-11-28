447035e9046ad12074a6044a9ea701e3b4f3d86e
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-28 11:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this.species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Setter for species (private variable)
+  set species(value) {
+    this._species = value;
+  }
+
+  // Method to make sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+class Cat extends Animal {
+  // Constructor calls the Animal constructor
+  constructor(species) {
+    super(species);
+  }
+
+  // Method specific to Cat class
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  // Constructor calls the Animal constructor
+  constructor(species) {
+    super(species);
+  }
+
+  // Method specific to Dog class
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example Usage:
+
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
+
 
 // Do not change the code below this line
 window.Animal = Animal;
